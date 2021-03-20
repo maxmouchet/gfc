edd1ed02bdea647bb8147a31a47286ee2544343d
Maxime Mouchet <max@maxmouchet.com>
2021-03-20 19:13:45 +0100

    v0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pygfc",
-    version="0.1.1",
+    version="0.1.2",
     author="Maxime Mouchet",
     author_email="max@maxmouchet.com",
     description="(Partial) implementation of a Generalized-Feistel Cipher for generating random permutations.",
